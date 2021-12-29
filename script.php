c6a6adabf631926be76f6e831bf87c6ec092f895
Ruan <ruanthiago67@gmail.com>
2021-12-28 21:53:18 -0300

    aprendendo ténicaas de validação

--- script.php
+++ script.php
@@ -1,28 +1,42 @@
 <?php
 
 $categorias = [];
-$categorias[] = "Infantil";
-$categorias[] = "Adolescente";
-$categorias[] = "Adulto";
+$categorias[0] = "Infantil";
+$categorias[1] = "Adolescente";
+$categorias[2] = "Adulto";
 
-$nome =$_POST['nome'];
+$nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-var_dump($nome);
-var_dump($idade);
+if(empty($nome)){
+    echo 'digite um nome para a pessoa';
+    return;
+}
+if(strlen($nome)<3){
+    echo 'o  nome tem que ter mais de 3 caracteres';
+    return;
+}
+if(strlen($nome)> 11){
+    echo 'o nome tem que ter menos que 11 caracteres';
+    return;
+}
 
 
-if($idade >= 6 && $idade <= 12 ){
-    for($i=2; $i < count($cagegorias);$i++){
-        if($categorias[$i]=="infantil");
-        echo "o nadador". $nome. "compete na categoria infantil";
+//var_dump($nome);
+//var_dump($idade);
+
+
+if($idade >=6 && $idade <=12 ){
+    for($i=2; $i < count($categorias);$i++){
+        if($categorias[$i]==="infantil");
+        echo "o nadador(a) ". $nome. " compete na categoria infantil";
     }
 }
 else if ($idade >=13 && $idade<=18){
-    for($i=2; $i < count($cagegorias);$i++){
-        if($categorias[$i]=="adolescente");
-        echo "o nadador". $nome. "compete na categoria adolescente";
+    for($i=2; $i < count($categorias);$i++){
+        if($categorias[$i]==="adolescente");
+        echo "o nadador(a) ". $nome. " compete na categoria adolescente";
     }
 }else {
-    echo "a pessoa".$nome."compete na categoria adulto";
+    echo "o nadador(a) ".$nome." compete na categoria adulto";
 }
